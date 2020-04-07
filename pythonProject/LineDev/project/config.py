648ae1166c1d0716a81b320f235f783f51c42e28
teakongnoo <tea.kongnoo@gmail.com>
2020-04-07 11:39:02 +0700

    First Python Commit

---
+++ pythonProject/LineDev/project/config.py
@@ -0,0 +1,7 @@
+'''channel_secret = 'e0224af3ad9d32945ed05e13af9fa50e'
+channel_access_token= "ZISFnwaOpJ0iQiytqHn8qhQiz2lYNo3HlD+tYUz3d+hGcNqqd1Gd+/gjB2qOACzmd1ZpVkAZVeOHoGpjvG+1xTyKfo2HbspyZmjvfgcyrIGjGfnr4bLon0WkfA+DVlKtBgze7hu4fLhgyl0PitOkMQdB04t89/1O/w1cDnyilFU="
+basic_id = "@929tubvc" '''
+
+channel_ID ="1653341909"
+channel_secret = "e0224af3ad9d32945ed05e13af9fa50e"
+channel_access_oken="ZISFnwaOpJ0iQiytqHn8qhQiz2lYNo3HlD+tYUz3d+hGcNqqd1Gd+/gjB2qOACzmd1ZpVkAZVeOHoGpjvG+1xTyKfo2HbspyZmjvfgcyrIGjGfnr4bLon0WkfA+DVlKtBgze7hu4fLhgyl0PitOkMQdB04t89/1O/w1cDnyilFU="
